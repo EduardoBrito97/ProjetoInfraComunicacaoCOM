b8c66ff4eacad6230df0c8f2e585042529097c05
Lucas Pontes de Albuquerque <lpa@cin.ufpe.br>
2017-12-03 20:51:54 -0300

    Add files via upload

--- Questao2.py
+++ Questao2.py
@@ -8,7 +8,6 @@ class Package():
 	UserName = ''
 	Message = ''
 	Acknowledge = 0
-	SequenceNumber = 0
 	CheckSum = 0
 
 def CalculateCheckSum(message):
@@ -25,10 +24,8 @@ def GetUserName():
 	return _delimiter + _username
 
 def GetAcknowledge():
-	return  _delimiter + '555'
-
-def GetSequenceNumber():
-	return _delimiter + '222'
+	global setAck
+	return  _delimiter + setAck
 
 def GetChecksum(message):
 	return _delimiter + CalculateCheckSum(message)
@@ -37,18 +34,13 @@ def GetMessage(message):
 	return _delimiter + message
 
 def CreateMessage(message):
-	return GetControlBit() + GetUserName() + GetAcknowledge() + GetSequenceNumber() + GetChecksum(message) + GetMessage(message)
+	return GetControlBit() + GetUserName() + GetAcknowledge() + GetChecksum(message) + GetMessage(message)
 
 def CreatePackage (rcvdMessage):
 	rcvdPackage = Package()
-	controlBit = ''
-	ack = ''
-	seq = ''
-	check = ''
-	controlBit, rcvdPackage.UserName, ack, sqe, check, rcvdPackage.Message = rcvdMessage.split(_delimiter)
+	controlBit, rcvdPackage.UserName, ack, check, rcvdPackage.Message = rcvdMessage.split(_delimiter)
 	rcvdPackage.ControlBit = controlBit
 	rcvdPackage.Acknowledge = ack
-	rcvdPackage.SequenceNumber = seq
 	rcvdPackage.CheckSum = check
 	return rcvdPackage
 
@@ -71,16 +63,18 @@ def DecodifyMessage(rcvdMessage, client):
 	return rcvdPackage
 
 def PrintMessage(rcvdDatagram, client):
+	global messages
 	global _senders
 	global _control
 	global _socket
 	rcvdPackage = DecodifyMessage(rcvdDatagram, client)
 	if rcvdPackage.ControlBit == '1':
-		aux, aux2 = rcvdPackage.Message.split(',')
-		aux = aux[2:len(aux)-1]
-		aux2 = int(aux2[:len(aux2)-1])
-		_senders.append((aux,aux2))
-	if client not in _senders:
+		ip, portR = rcvdPackage.Message.split(',')
+		ip = ip[2:len(ip)-1]
+		portR = int(portR[:len(portR)-1])
+		_senders.append((ip,portR))
+
+	elif client not in _senders:
 			_control = 1
 			_senders.append(client)
 			for contact in _senders:
@@ -88,7 +82,17 @@ def PrintMessage(rcvdDatagram, client):
 					_socket.sendto(CreateMessage(str(client)), contact)
 					_socket.sendto(CreateMessage(str(contact)), client)
 			_control = 0
-	print  rcvdPackage.UserName, " says: ", rcvdPackage.Message
+	else:
+		print  rcvdPackage.UserName, " says: ", rcvdPackage.Message
+	if rcvdPackage.Acknowledge == '0':
+		setAck = '1'
+		SendPackage(recvdPackage.Message,client)
+	elif rcvdPackage.Acknowledge == '1':
+		setAck = '0'
+		for aux in messages:
+			if aux[1] == client[0]:
+				SendPackage(aux[0],client)
+
 
 def ReceiveMessage(name,s):
 	while True:
@@ -96,12 +100,15 @@ def ReceiveMessage(name,s):
 		PrintMessage(rcvdDatagram, client)
 
 def CallServer():
+	global messages
 	global _senders
 	while 1:
 		message = raw_input()
 		print str(len(_senders))
 		for i in _senders:
+			setAck = '0'
 			SendPackage(message,i)
+			messages.append((message,i))
 
 
 def AmIServer():
@@ -117,17 +124,20 @@ def AmIServer():
 		return 0
 
 #Program:
+setAck = '0'
+messages = []
 _username = raw_input("What's your name? ")
 _port = 12000
 _control = 0
 _senders = []
 _socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 _delimiter = '@#!@!#!@!$!#!#@#!!'
-_ERROR = 'SYSTEM' + GetAcknowledge() + GetSequenceNumber() + GetChecksum('Something went wrong.') + GetMessage('Something went wrong.')
+_ERROR = 'SYSTEM' + GetAcknowledge() + GetChecksum('Something went wrong.') + GetMessage('Something went wrong.')
 new_cont = 0
 conversation = raw_input("Do you want start a conversation(y/n)")
 
-threading.Thread(target=ReceiveMessage, args=("rec",_socket)).start()
+for i in range(5):
+	threading.Thread(target=ReceiveMessage, args=("rec",_socket)).start()
 
 AmIServer()
 CallServer()
