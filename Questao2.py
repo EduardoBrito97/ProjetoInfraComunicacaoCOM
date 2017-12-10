1345d4e2b9200ec41395967531446b75f4d91117
danielclm14 <32872663+danielclm14@users.noreply.github.com>
2017-12-10 18:11:47 -0300

    Last Update Q2

--- Questao2.py
+++ Questao2.py
@@ -18,14 +18,14 @@ def CalculateCheckSum(message):
 	return str(CheckSum)
 def GetControlBit():
 	global _control
-	print str(_control)
+	#print str(_control)
 	return str(_control)
 
-def GetUserName():
-	return _delimiter + _username
+def GetUserName(username):
+	return _delimiter + username
 
-def GetAcknowledge():
-	return  _delimiter + '555'
+def GetAcknowledge(ack):
+	return  _delimiter + str(ack)
 
 def GetSequenceNumber():
 	return _delimiter + '222'
@@ -36,8 +36,8 @@ def GetChecksum(message):
 def GetMessage(message):
 	return _delimiter + message
 
-def CreateMessage(message):
-	return GetControlBit() + GetUserName() + GetAcknowledge() + GetSequenceNumber() + GetChecksum(message) + GetMessage(message)
+def CreateMessage(message, user, ack):
+	return GetControlBit() + GetUserName(user) + GetAcknowledge(ack) + GetSequenceNumber() + GetChecksum(message) + GetMessage(message)
 
 def CreatePackage (rcvdMessage):
 	rcvdPackage = Package()
@@ -45,28 +45,43 @@ def CreatePackage (rcvdMessage):
 	ack = ''
 	seq = ''
 	check = ''
-	controlBit, rcvdPackage.UserName, ack, sqe, check, rcvdPackage.Message = rcvdMessage.split(_delimiter)
+	controlBit, rcvdPackage.UserName, ack, seq, check, rcvdPackage.Message = rcvdMessage.split(_delimiter)
 	rcvdPackage.ControlBit = controlBit
 	rcvdPackage.Acknowledge = ack
 	rcvdPackage.SequenceNumber = seq
 	rcvdPackage.CheckSum = check
 	return rcvdPackage
 
-def SendPackage (message,client):
-	_socket.sendto(CreateMessage(message), client)
+def GetMSGwfError():
+	return GetControlBit() + GetUserName('sYSERrORmSG') + GetAcknowledge(_myacknum) + GetSequenceNumber() + GetChecksum('SmetHing uet Wrromng.') + GetMessage('Something went wrong.')
+
+def SendMSGwfError(client):
+	_socket.sendto(GetMSGwfError(), client)
+
+def SendPackage (message, client, ack, user):
+	_socket.sendto(CreateMessage(message, user, ack), client)
 
 def isCorrupt(rcvdPackage):
 	return rcvdPackage.CheckSum != CalculateCheckSum(rcvdPackage.Message)
 
-def TryAgain (client):
-	_socket.sendto(_ERROR, client)
-	return DecodifyMessage(ReceiveMessage())
-
+def TryAgain (client, ack):
+	#_socket.sendto(_ERROR, client)
+	SendPackage('Something went wrong.', client, ack, 'SYSERRORMSG')
+	
 def DecodifyMessage(rcvdMessage, client):
+	global _lastMessage
 	rcvdPackage = CreatePackage(rcvdMessage)
-
 	if isCorrupt(rcvdPackage):
-		rcvdPackage = TryAgain (client)
+		TryAgain (client, rcvdPackage.Acknowledge)
+		
+	if (rcvdPackage.UserName == 'SYSERRORMSG'):
+		SendPackage(_lastMessage[len(_lastMessage)-1], client, _myacknum, _username)
+		#e reinicializa temporizador
+		
+
+	#if (rcvdPackage.UserName == 'SYSACKMSG'):
+		#para temporizador
+
 
 	return rcvdPackage
 
@@ -75,20 +90,24 @@ def PrintMessage(rcvdDatagram, client):
 	global _control
 	global _socket
 	rcvdPackage = DecodifyMessage(rcvdDatagram, client)
-	if rcvdPackage.ControlBit == '1':
-		aux, aux2 = rcvdPackage.Message.split(',')
-		aux = aux[2:len(aux)-1]
-		aux2 = int(aux2[:len(aux2)-1])
-		_senders.append((aux,aux2))
-	if client not in _senders:
-			_control = 1
-			_senders.append(client)
-			for contact in _senders:
-				if contact != client:
-					_socket.sendto(CreateMessage(str(client)), contact)
-					_socket.sendto(CreateMessage(str(contact)), client)
-			_control = 0
-	print  rcvdPackage.UserName, " says: ", rcvdPackage.Message
+	if (rcvdPackage.UserName != 'SYSACKMSG'):
+		if (rcvdPackage.UserName != 'SYSERRORMSG'):
+			if (rcvdPackage.UserName != 'sYSERrORmSG'):
+				SendPackage('Acknowledge message ok.', client, rcvdPackage.Acknowledge, 'SYSACKMSG')
+				if (rcvdPackage.ControlBit == '1'):
+					aux, aux2 = rcvdPackage.Message.split(',')
+					aux = aux[2:len(aux)-1]
+					aux2 = int(aux2[:len(aux2)-1])
+					_senders.append((aux,aux2))
+				if client not in _senders:
+						_control = 1
+						_senders.append(client)
+						for contact in _senders:
+							if contact != client:
+								_socket.sendto(CreateMessage(str(client), _username, _myacknum), contact)
+								_socket.sendto(CreateMessage(str(contact), _username, _myacknum), client)
+						_control = 0
+				print  rcvdPackage.UserName, " says: ", rcvdPackage.Message
 
 def ReceiveMessage(name,s):
 	while True:
@@ -97,11 +116,22 @@ def ReceiveMessage(name,s):
 
 def CallServer():
 	global _senders
+	global _lastMessage
+	global _myacknum
+	global conversation
 	while 1:
-		message = raw_input()
-		print str(len(_senders))
-		for i in _senders:
-			SendPackage(message,i)
+		if (conversation == 'error'):
+			for z in _senders:
+				SendMSGwfError(z)
+			conversation = raw_input('conversation error again?')
+
+		else:
+			message = raw_input()
+			_lastMessage.append(message)
+			_myacknum = _myacknum + 1
+			#print str(len(_senders))
+			for i in _senders:
+				SendPackage(message, i, _myacknum, _username)
 
 
 def AmIServer():
@@ -117,18 +147,22 @@ def AmIServer():
 		return 0
 
 #Program:
+_lastMessage = []
+_lastMessage.append('olar')
+_myacknum = 0
 _username = raw_input("What's your name? ")
 _port = 12000
 _control = 0
 _senders = []
 _socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 _delimiter = '@#!@!#!@!$!#!#@#!!'
-_ERROR = 'SYSTEM' + GetAcknowledge() + GetSequenceNumber() + GetChecksum('Something went wrong.') + GetMessage('Something went wrong.')
+#_ERROR = GetControlBit() + GetUserName('sYSERrORmSG') + GetAcknowledge(_myacknum) + GetSequenceNumber() + GetChecksum('SmetHing uet Wrromng.') + GetMessage('Something went wrong.')
 new_cont = 0
 conversation = raw_input("Do you want start a conversation(y/n)")
-
-threading.Thread(target=ReceiveMessage, args=("rec",_socket)).start()
+for u in range(5):
+	threading.Thread(target=ReceiveMessage, args=("rec",_socket)).start()
 
 AmIServer()
 CallServer()
+
 
